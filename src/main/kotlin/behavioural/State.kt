6c3f44c9e70d602d3b0ff0b698a96473a4d9ee80
Mukul Jangir <mukul.jangir372@gmail.com>
2023-06-16 16:14:14 +0530

    state pattern

---
+++ src/main/kotlin/behavioural/State.kt
@@ -0,0 +1,25 @@
+package behavioural
+
+/**
+ * State is one of simplest behavioural design pattern in kotlin
+ * which is used for presenting a state.
+ */
+
+private sealed interface LoginState {
+    object Active : LoginState
+    object InActive : LoginState
+    object Idle : LoginState
+}
+
+private class AuthPresenter {
+    private var state: LoginState = LoginState.Idle
+
+    fun login() {
+        state = LoginState.Active
+    }
+
+    fun logout() {
+        state = LoginState.InActive
+    }
+}
+
